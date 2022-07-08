4874ce2d4ebf7a7c9ecd04c49bdd97f07698682e
Walid Moheyeldin <walid.ibrahim.moheyeldin@hotmail.com>
2022-07-08 09:55:27 +0200

    Update Program.cs

--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,8 +1,8 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<IEmployeeService, EmployeeService>();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.MapGet("/", (HttpContext http) 
     => $".NET 6 Minimal Api Demo! => {http.Request.Scheme}://{http.Request.Host.Value}/api/Employees");
